97b092e8db27934e545e13871d6a7a2fa39422f7
Marc Boanas <marcjohnboanas@gmail.com>
2015-04-19 18:40:01 +0100

    reset

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,13 +1,1 @@
-var app = angular.module('myapp', []);
 
-app.controller('MainCtrl', function($scope) {
-  $scope.name = 'World';
-  $scope.items = [{name: "One"}];
-  setTimeout(function() {
-    $scope.$apply(function() {
-     $scope.items[0].lateLoader = 'i just loaded';
-    });
-
-
-  }, 10000);
-});
